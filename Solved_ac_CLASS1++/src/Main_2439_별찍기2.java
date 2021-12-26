2019d7e16c0953c77f05b3b8e0d6f303d0567945
chang0-0 <bigyoung8375@naver.com>
2021-12-26 15:50:01 +0900

    complete

--- Solved_ac_CLASS1++/src/Main_2439_별찍기2.java
+++ Solved_ac_CLASS1++/src/Main_2439_별찍기2.java
@@ -10,12 +10,14 @@ public class Main_2439_별찍기2 {
 		
 		// count 만큼의 자릿수는 계속 나와야 됨
 		for(int i=count; i>0; i--) {
+			
 			// 공백넣는 반복문
 			for(int k=1; k<i; k++) {
 				System.out.print(" ");
 			}
 			
-			for(int j=0; j<(count - i) + 1; j++) {
+			// 별을 집어 넣는 반복문
+			for(int j=0; j<=(count - i); j++) {
 				System.out.print("*");
 			}
 			
