7949653a3c9472d110f892c609e861805f62255f
chang0-0 <bigyoung8375@naver.com>
2021-12-27 14:57:20 +0900

    complete

---
+++ Solved_ac_CLASS1++/src/Main_2675_문자열반복.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main_2675_문자열반복 {
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("res/input_bj_2675.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		
+	}
+}
