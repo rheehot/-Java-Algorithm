ba7edd350c83717e7cb6b506295c09baeac57ce3
chang0-0 <bigyoung8375@naver.com>
2021-12-28 16:36:24 +0900

    complete

--- Solved_ac_CLASS2/src/Main_1018_체스판다시칠하기.java
+++ Solved_ac_CLASS2/src/Main_1018_체스판다시칠하기.java
@@ -22,18 +22,51 @@ public class Main_1018_체스판다시칠하기 {
 								{"W","B","W","B","W","B","W","B"}};
 	
 	// 최솟값을 계산하는 메소드
-	public static void find(int x, int y) {
-		// x를 증가시키며 탐색하는 반복문
-		for(int j=0; j<x+8; j++) {
-			for(int k=0; k<y+8; k++) {
-				
+	public static int find(String[][] temp) {
+		int count = 0;
+		int i=0;
+		int j=0;
+		
+		char first = temp[0][0].charAt(0);
+		System.out.println("first : " + first);
+		
+		if(first == 'B') {
+			while(i<8) {
+				j = 0;
+				while(j<8) {
+			
+					if(temp[i][j].charAt(0) != bStart[i][j].charAt(0)) {
+						count ++;
+					}
+					j++;
+				}
+				i++;
 			}
 		}
+		else if(first == 'W') {
+			while(i<8) {
+				j = 0;
+				while(j<8) {
+					if(temp[i][j].charAt(0) != wStart[i][j].charAt(0)) {
+						count ++;
+					}
+					j++;
+				}
+				i++;
+			}
+		}
+		
+		System.out.println(" count : " + count);
+		
+		return count;
 	}
 	
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("res/input_bj_1018.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		int min = Integer.MAX_VALUE;
+		int num = 0;
 		
 		String arr[] = br.readLine().split(" ");
 		int N = Integer.parseInt(arr[0]);
@@ -43,27 +76,45 @@ public class Main_1018_체스판다시칠하기 {
 		
 		// 입력받은 체스판 생성
 		for(int i=0; i<chess.length; i++) {
-			String temp[] = br.readLine().split("");
-			for(int j=0; j<temp.length; j++) {
-				chess[i][j] = temp[j];
+			String arr2[] = br.readLine().split("");
+			for(int j=0; j<arr2.length; j++) {
+				chess[i][j] = arr2[j];
 			}
 		}
 			
-		// 세로축 반복
-		for(int y=0; y<M-8; y++) {
-			System.out.println("세로축 반복 횟수 : " + y);
-			// 가로축 반복
-			int temp = y;
-			for(int i=0; i<N-8; i++) {
+		// 1. 세로를 기준으로 옆으로 밀어내면서 체스판을 생성하고
+		// 2. 세로를 기준으로 더이상 밀어낼 공간이 없다면 밑으로 한칸 내려가서 위 과정을 다시 반복
+		for(int x=0; x<N-8; x++) {
+			String temp[][] = new String[8][8];
 
-				for(int j=i; j<i+8; j++) {
-					System.out.print(chess[j][temp]);
-					temp ++;
+			for(int y=0; y<M-8; y++) {
+				int g = 0;
+				for(int i=x; i<x+8; i++) {
+					int s = 0;
+					for(int j=y; j<y+8; j++) {
+						System.out.println(chess[i][j]);
+						//System.out.println(" g: " + g + " s: " + s);
+						temp[g][s] = chess[i][j];
+						
+						if(g == 7 && s == 7) {
+							num = find(temp);
+							System.out.println(" num : " + num);
+							
+							if(num < min) {
+								min = num;
+							}
+						}
+						
+						s++;
+					}
+					g++;
+					System.out.println(" ");
 				}
-				temp = 0;
-				System.out.println("");
+				System.out.println(" ");
 			}
 		}
 
+		System.out.println("min : " + min);
+		
 	}
 }
