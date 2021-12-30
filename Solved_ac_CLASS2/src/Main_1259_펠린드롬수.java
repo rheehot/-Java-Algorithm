7c43be32a566397828ce8498dface10dfa74d398
chang0-0 <bigyoung8375@naver.com>
2021-12-30 13:53:08 +0900

    complete

--- Solved_ac_CLASS2/src/Main_1259_펠린드롬수.java
+++ Solved_ac_CLASS2/src/Main_1259_펠린드롬수.java
@@ -3,28 +3,40 @@ import java.util.*;
 
 public class Main_1259_펠린드롬수 {
 	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("res/input_bj_2675.txt"));
+		System.setIn(new FileInputStream("res/input_bj_1259.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		LinkedList<Character> linkedList = new LinkedList<>();
+		LinkedList<Character> linkedList2 = new LinkedList<>();
 		
-		int loopCount = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<loopCount; i++) {
-			String arr[] = br.readLine().split(" ");
-			int loopCount2 = Integer.parseInt(arr[0]);
-			int strLength = arr[1].length();
-			String str = arr[1];
-			String temp = "";
+		// 0 일때까지 반복
+		int num;
+		while((num = Integer.parseInt(br.readLine())) != 0) {
+			String temp = Integer.toString(num);
 			
-			for(int j=0; j<strLength; j++) {
-				for(int k=0; k<loopCount2; k++) {
-					temp += str.charAt(i);
-				}
-				
+			for(int i=0; i<temp.length(); i++) {
+				linkedList.add(temp.charAt(i));
 			}
-			System.out.print(temp);
-			System.out.println(" ");
-		} 
-		
+			
+			for(int j=temp.length()-1; j>=0; j--) {
+				linkedList2.add(temp.charAt(j));
+			}
+						
+			for(char ch : linkedList) {
+				char ch2 = linkedList2.poll();
+				
+				if(ch != ch2) {
+					System.out.println("no");
+					break;
+				}
+				else if(linkedList2.isEmpty()) {
+					System.out.println("yes");
+				}
+			}
+			
+			linkedList.clear();
+			linkedList2.clear();
+		}
+
 		br.close();
 	}
 }
